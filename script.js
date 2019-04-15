f156703fbfdf4c9db438c1ca3420394353c7dbbe
gabe fantacone <gabrielfantacone@students.berkeley.net>
2019-04-15 14:35:27 -0700

    Get it to run by itself

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-function calcPayments(balance, rate){
-    var rateAdjusted = rate / 12;
+function calcPayments(){
+    var rateAdjusted = 0.1499 / 12;
+    var balance = 2500;
     var month = 0;
+    var total = 0;
     var monthly;
     while (balance > 20){
         if (balance / 20 > 20){
-            monthly = (balance/20)*(1 + rateAdjusted);
+            monthly = balance / 20;
         } else {
             monthly = 20;
         }
         total += monthly;
         balance -= monthly;
+        balance *= (1 + rateAdjusted);
+        month++;
     }
+    total += balance;
+    month++;
+    console.log("It will take " + month + " months to pay off the debt, and you will have paid $" + total + " in total.");
 }
+
